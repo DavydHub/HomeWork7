c1e41f7e85c38195c54da719870bda1d3cdc3ce2
Oleksandr Davydenko <oleksandr.davydenko3@gmail.com>
2020-10-09 01:05:39 +0300

    added if !== ""

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ const list = document.querySelector(".least");
 
 addButton.addEventListener("click", () => {
   const textDate = document.getElementById("inp").value;
-  const ul = document.querySelector(".least");
-  ul.insertAdjacentHTML(
-    "afterBegin",
-    `<li class="list_item">
+
+  if (textDate !== "") {
+    const ul = document.querySelector(".least");
+    ul.insertAdjacentHTML(
+      "afterBegin",
+      `<li class="list_item">
     ${textDate} <button class="item_btn">+</button>
      </li>`
-  );
-  console.log(textDate);
+    );
+  } else console.log(textDate);
 });
 
 console.log(list);
